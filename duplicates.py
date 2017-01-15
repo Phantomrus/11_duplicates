59891c4e9bcbec425fee261af43a84ca6e406a05
Phantomrus <phantomrus@gmail.com>
2017-01-15 05:33:03 +0300

    Third iteration. The main function is simplified

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,14 @@
 import os
 from collections import defaultdict
 
+
+def enter_path():
+    directory = input("Введите папку для поиска дубликатов:\n")
+    while not os.path.isdir(directory):
+            directory = input("Такой директории не существует. Пожалуйста, введите корректный путь:\n")
+    return directory
+
+
 def register_creation(directory):
     
     register = defaultdict(list)
@@ -14,6 +22,7 @@ def register_creation(directory):
 
     return register
 
+
 def search_doubles(register):
     # list_of_doubles [[имя, размер, [расположение 1, расположение 2 ... расположение n]
     #           [имя, размер, [расположение 1, расположение 2 ... расположение n]]]
@@ -24,22 +33,7 @@ def search_doubles(register):
     return list_of_doubles
 
 
-def print_doubles(list_of_doubles):
-    for num, element in enumerate(list_of_doubles, start =1):
-        print("\nДублированный файл номер %s\nИмя файла: %s\nРазмер файла: %s\nВсе местоположения файла:"\
-            % (num, element[0], element[1]))
-        for path in element[2]:
-            print(path)
-
-if __name__ == '__main__':
-
-    directory = input("Введите папку для поиска дубликатов:\n")
-    while not os.path.isdir(directory):
-        directory = input("Такой директории не существует. Пожалуйста, введите корректный путь:\n")
-
-    register = register_creation(directory)
-
-    list_of_doubles = search_doubles(register)
+def print_options(list_of_doubles):
     if list_of_doubles:
         print("Реестр дублированных файлов готов.")
         while True:
@@ -52,5 +46,22 @@ if __name__ == '__main__':
                 print("\nВведено неверное значение.\n")
     else:
         print("Дублированные файлы в директории не найдены.")
+
+
+def print_doubles(list_of_doubles):
+    for num, element in enumerate(list_of_doubles, start =1):
+        print("\nДублированный файл номер %s\nИмя файла: %s\nРазмер файла: %s\nВсе местоположения файла:"\
+            % (num, element[0], element[1]))
+        for path in element[2]:
+            print(path)
+
+
+if __name__ == '__main__':
+
+    directory = enter_path()
+    register = register_creation(directory)
+
+    list_of_doubles = search_doubles(register)
+    print_options(list_of_doubles)
 
     print("\nПрограмма завершена.")
