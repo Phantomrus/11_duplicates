4f804d88f3ce1eeec7fc39d5f49f86d209de5085
Phantomrus <phantomrus@gmail.com>
2017-01-22 08:34:16 +0300

    Исправлены замечания
    Добавлено использование аргументов при вызове скрипта;
    Функция register_creation переименована в creation_of_the_register;
    Добавлено использование list comprehensions при создании словарей.

--- duplicates.py
+++ duplicates.py
@@ -1,15 +1,20 @@
 import os
+import argparse
 from collections import defaultdict
 
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path')
+    return parser
 
-def enter_path():
-    directory = input("Введите папку для поиска дубликатов:\n")
+
+def check_path(directory):
     while not os.path.isdir(directory):
-            directory = input("Такой директории не существует. Пожалуйста, введите корректный путь:\n")
+            directory = input("Указанной вами директории не существует. Пожалуйста, введите корректный путь:\n")
     return directory
 
 
-def register_creation(directory):
+def creation_of_the_register(directory):
     
     register = defaultdict(list)
     # Словарь {(Имя файла, размер): [расположение1, расположение2]}
@@ -24,22 +29,21 @@ def register_creation(directory):
 
 
 def search_doubles(register):
-    # list_of_doubles [[имя, размер, [расположение 1, расположение 2 ... расположение n]
-    #           [имя, размер, [расположение 1, расположение 2 ... расположение n]]]
-    list_of_doubles = []
-    for key in register:
-        if len(register[key]) > 1:
-            list_of_doubles.append([key[0], key[1], register[key]])
+    # list_of_doubles {(имя, размер): [расположение 1, расположение 2 ... расположение n]
+    #           (имя, размер): [расположение 1, расположение 2 ... расположение n]]}
+
+    list_of_doubles = {(key[0], key[1]): register[key] for key in register if len(register[key]) > 1}
     return list_of_doubles
 
 
 def print_options(list_of_doubles):
     if list_of_doubles:
-        print("Реестр дублированных файлов готов.")
+        print("Дублированные файлы найдены. Реестр дублированных файлов готов.")
         while True:
             choice = input("\nВведите 1, если хотите вывести список дублей на экран.\nВведите 0, если хотите завершить программу.\n")
             if choice == "1":
                 print_doubles(list_of_doubles)
+                break
             elif choice == "0":
                 break
             else:
@@ -49,17 +53,19 @@ def print_options(list_of_doubles):
 
 
 def print_doubles(list_of_doubles):
-    for num, element in enumerate(list_of_doubles, start =1):
+    for num, element in enumerate(list_of_doubles.keys(), start =1):
         print("\nДублированный файл номер %s\nИмя файла: %s\nРазмер файла: %s\nВсе местоположения файла:"\
             % (num, element[0], element[1]))
-        for path in element[2]:
+        for path in list_of_doubles[element]:
             print(path)
 
 
 if __name__ == '__main__':
 
-    directory = enter_path()
-    register = register_creation(directory)
+    parser = createParser()
+    namespace = parser.parse_args()
+    directory = check_path(namespace.path)
+    register = creation_of_the_register(directory)
 
     list_of_doubles = search_doubles(register)
     print_options(list_of_doubles)
