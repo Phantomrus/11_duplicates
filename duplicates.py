293f24df31205f65a310b2932ff3a89f4c9072f3
Phantomrus <phantomrus@gmail.com>
2017-01-22 10:39:43 +0300

    Исправлены замечания
    Скорректированы названия некоторых переменных. 
    Убрана опция выбора дальнейшего действия - скрипт теперь всегда выводит список дубликатов, не спрашивая о необходимости.

--- duplicates.py
+++ duplicates.py
@@ -2,72 +2,57 @@ import os
 import argparse
 from collections import defaultdict
 
-def create_parser():
+
+def creation_of_the_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path')
     return parser
 
 
-def check_path(directory):
-    while not os.path.isdir(directory):
-            directory = input("Указанной вами директории не существует. Пожалуйста, введите корректный путь:\n")
-    return directory
+def check_of_a_directory(directory_for_check):
+    while not os.path.isdir(directory_for_check):
+        directory_for_check = input("Указанной вами директории не существует. Пожалуйста, введите корректный путь:\n")
+    return directory_for_check
 
 
-def creation_of_the_register(directory):
-    
-    register = defaultdict(list)
-    # Словарь {(Имя файла, размер): [расположение1, расположение2]}
+def creation_of_the_register(directory_for_check):  
+    register_of_files = defaultdict(list)
 
-    for root, dirs, files in os.walk(directory):
+    for root_directory, directorys, files in os.walk(directory_for_check):
         for file_name in files:
-            path = os.path.join(root,file_name)
-            size_file = os.path.getsize(path)
-            register[(file_name, size_file)].append(path)
+            path_to_file = os.path.join(root_directory,file_name)
+            size_file = os.path.getsize(path_to_file)
+            register_of_files[(file_name, size_file)].append(path_to_file)
 
-    return register
+    return register_of_files
 
 
-def search_doubles(register):
-    # list_of_doubles {(имя, размер): [расположение 1, расположение 2 ... расположение n]
-    #           (имя, размер): [расположение 1, расположение 2 ... расположение n]]}
-
-    list_of_doubles = {(key[0], key[1]): register[key] for key in register if len(register[key]) > 1}
+def search_doubles(register_of_files):  
+    list_of_doubles = {(key[0], key[1]): register_of_files[key] for key in register_of_files if len(register_of_files[key]) > 1}
     return list_of_doubles
 
 
-def print_options(list_of_doubles):
+def print_doubles(list_of_doubles):
     if list_of_doubles:
-        print("Дублированные файлы найдены. Реестр дублированных файлов готов.")
-        while True:
-            choice = input("\nВведите 1, если хотите вывести список дублей на экран.\nВведите 0, если хотите завершить программу.\n")
-            if choice == "1":
-                print_doubles(list_of_doubles)
-                break
-            elif choice == "0":
-                break
-            else:
-                print("\nВведено неверное значение.\n")
+        print("Найдены следующие дублированные файлы:")
+        for num, element in enumerate(list_of_doubles.keys(), start =1):
+            print("\nДублированный файл номер %s\nИмя файла: %s\nРазмер файла: %s\nВсе местоположения файла:"\
+                % (num, element[0], element[1]))
+            for path_to_file in list_of_doubles[element]:
+                print(path_to_file)
     else:
         print("Дублированные файлы в директории не найдены.")
 
 
-def print_doubles(list_of_doubles):
-    for num, element in enumerate(list_of_doubles.keys(), start =1):
-        print("\nДублированный файл номер %s\nИмя файла: %s\nРазмер файла: %s\nВсе местоположения файла:"\
-            % (num, element[0], element[1]))
-        for path in list_of_doubles[element]:
-            print(path)
-
-
 if __name__ == '__main__':
 
-    parser = create_parser()
-    namespace = parser.parse_args()
-    directory = check_path(namespace.path)
-    register = creation_of_the_register(directory)
+    parser = creation_of_the_parser()
+    arguments_of_script = parser.parse_args()
 
-    list_of_doubles = search_doubles(register)
-    print_options(list_of_doubles)
+    directory_for_check = check_of_a_directory(arguments_of_script.path)
+    register_of_files = creation_of_the_register(directory_for_check)
+
+    list_of_doubles = search_doubles(register_of_files)
+    print_doubles(list_of_doubles)
 
     print("\nПрограмма завершена.")
